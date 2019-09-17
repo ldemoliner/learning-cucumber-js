4f7cea578f8c4be2189a2f75e1741c01d364e9c7
Demoliner <ld72635@sa.agcocorp.com>
2019-09-17 08:43:05 -0300

    Adicionando exerciseBO

--- features/step-definitions/add_exercise_to_student_steps.js
+++ features/step-definitions/add_exercise_to_student_steps.js
@@ -1,19 +1,20 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 const StudentBO = require('../../src/studentBO');
+const ExerciseBO = require('../../src/exerciseBO');
 
 
-Given('the student {string} is registered', function (string) {
-  
+Given('the student {string} is registered', function (name) {
+  ExerciseBO.registerExercise(name);
 });
 
 
-When('I associate the exercise {string}', function (string) {
-  
+When('I associate the exercise {string}', function (exercise) {
+  this.exercise = ExerciseBO.queryExercise(exercise);
 });
 
 
 Then('{string} should have the following exercise:', function (string, docString) {
   const exercises = JSON.parse(docString);
-  console.log('AAAA',exercises);
+  assert.equal(this.exercises, docString);
 });
---
+++ src/exerciseBO.js
@@ -0,0 +1,4 @@
+module.exports = ExerciseBO = {
+    registerExercise: exercise => {},
+    queryExercise: exercise => {}
+}
